d5556d1be487141770f60a7c632fce2ba6dccd80
alexivxii <alex.munteanu58@gmail.com>
2022-04-06 13:06:46 +0300

    schimbare nume aplicatie + comentarii

--- app/src/main/java/com/example/kotthemes/MainActivity.kt
+++ app/src/main/java/com/example/kotthemes/MainActivity.kt
@@ -1,17 +1,33 @@
 package com.example.kotthemes
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import androidx.core.app.ActivityCompat
 import android.Manifest
 import android.content.pm.PackageManager
+import android.graphics.Color
+import android.media.MediaPlayer
+import android.media.MediaRecorder
+import android.os.Bundle
+import android.util.Log
 import android.widget.Button
-import androidx.core.content.ContextCompat
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import java.io.IOException
+
 
 class MainActivity : AppCompatActivity() {
 
     //contor pentru a numara cate permisiuni acceptate avem
     var permNumber = 0
+
+    //https://kotlinlang.org/docs/properties.html#late-initialized-properties-and-variables pentru motiv lateinit
+    //variabila de tip MediaRecorder
+     var mRecorder: MediaRecorder ?=null
+
+    //variabile de tip MediaPlayer
+    lateinit var mPlayer: MediaPlayer
+
+    //variabila pentru numele fisierului de stocat
+    lateinit var mFileName: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,13 +57,18 @@ class MainActivity : AppCompatActivity() {
             if(ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED ||
                 ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED ||
                 ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
-                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.RECORD_AUDIO,Manifest.permission.WRITE_EXTERNAL_STORAGE,Manifest.permission.READ_EXTERNAL_STORAGE), 100)
+                ActivityCompat.requestPermissions(
+                    this, arrayOf(
+                        Manifest.permission.RECORD_AUDIO,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                        Manifest.permission.READ_EXTERNAL_STORAGE
+                    ), 100
+                )
             else {
                 //in cazul in care avem deja permisiunile la apasarea butonului
                 permNumber=3
                 println("Permissions Already Granted")
             }
-
 
         }
 
@@ -59,9 +80,27 @@ class MainActivity : AppCompatActivity() {
             replayButtonRef.isEnabled = true
         }
 
+        //functiile asociate butoanelor
+        startButtonRef?.setOnClickListener{
+            startRecording()
+        }
+
+        stopButtonRef?.setOnClickListener {
+            stopRecording()
+        }
+
+        replayButtonRef?.setOnClickListener {
+            playRecording()
+        }
+
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+    //functia care este lansata la cererea permisiunilor
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
 
         val startButtonRef: Button = findViewById(R.id.startButton)
@@ -101,6 +140,61 @@ class MainActivity : AppCompatActivity() {
             stopButtonRef.isEnabled = true
             replayButtonRef.isEnabled = true
         }
+
+    }
+
+    fun startRecording(){
+
+        val textViewTitle: TextView = findViewById(R.id.textTitle)
+
+        //pathul+numele fisierului de stocat
+        mFileName="${externalCacheDir?.absolutePath}/KotlinAudioRecording.3gp"
+        //mFileName="${externalCacheDir?.absolutePath}/KotlinAudioRecording"
+
+        println(mFileName)
+
+        mRecorder = MediaRecorder()
+
+        //sursa sunetului
+        mRecorder!!.setAudioSource(MediaRecorder.AudioSource.MIC)
+
+        //formatul fisierului audio
+        mRecorder!!.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        //setarea encoderului pentru fisierul audio
+        mRecorder!!.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+        //setarea locatiei output-ului inregistrarii
+        mRecorder!!.setOutputFile(mFileName);
+
+
+        //pregatim variabila de tip MediaRecorder pentru inregistrare
+        try {
+            mRecorder!!.prepare()
+        } catch (e: IOException) {
+            Log.e("TAG", "prepare() failed")
+        }
+
+        mRecorder!!.start();
+        //schimbam culoarea titlului in rosu pentru a stii ca suntem in modul de recording
+        textViewTitle.setTextColor(Color.parseColor("#ff5243"))
+
+    }
+
+    //redarea inregistrarii
+    fun playRecording(){
+        mPlayer = MediaPlayer()
+        mPlayer.setDataSource(mFileName)
+        mPlayer.prepare();
+        mPlayer.start();
+    }
+
+    //oprirea inregistrarii
+    fun stopRecording(){
+        mRecorder!!.stop()
+        mRecorder!!.release()
+        mRecorder = null
+
+        val textViewTitle: TextView = findViewById(R.id.textTitle)
+        textViewTitle.setTextColor(Color.parseColor("#808080"))
 
     }
 
