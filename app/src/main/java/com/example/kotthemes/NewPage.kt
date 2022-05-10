f17f064b293c46f64c55c76501f98f5e47d1b901
alexivxii <alex.munteanu58@gmail.com>
2022-05-10 22:14:36 +0300

    adaugare exemplu fft

--- app/src/main/java/com/example/kotthemes/NewPage.kt
+++ app/src/main/java/com/example/kotthemes/NewPage.kt
@@ -13,6 +13,55 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import java.lang.Math.*
+
+class Complex(val re: Double, val im: Double) {
+    operator infix fun plus(x: Complex) = Complex(re + x.re, im + x.im)
+    operator infix fun minus(x: Complex) = Complex(re - x.re, im - x.im)
+    operator infix fun times(x: Double) = Complex(re * x, im * x)
+    operator infix fun times(x: Complex) = Complex(re * x.re - im * x.im, re * x.im + im * x.re)
+    operator infix fun div(x: Double) = Complex(re / x, im / x)
+    val exp: Complex by lazy { Complex(cos(im), sin(im)) * (cosh(re) + sinh(re)) }
+
+    override fun toString() = when {
+        b == "0.000" -> a
+        a == "0.000" -> b + 'i'
+        im > 0 -> a + " + " + b + 'i'
+        else -> a + " - " + b + 'i'
+    }
+
+    private val a = "%1.3f".format(re)
+    private val b = "%1.3f".format(abs(im))
+}
+
+object FFT {
+    fun fft(a: Array<Complex>) = _fft(a, Complex(0.0, 2.0), 1.0)
+    fun rfft(a: Array<Complex>) = _fft(a, Complex(0.0, -2.0), 2.0)
+
+    private fun _fft(a: Array<Complex>, direction: Complex, scalar: Double): Array<Complex> =
+        if (a.size == 1)
+            a
+        else {
+            val n = a.size
+            require(n % 2 == 0, { "The Cooley-Tukey FFT algorithm only works when the length of the input is even." })
+
+            var (evens, odds) = Pair(emptyArray<Complex>(), emptyArray<Complex>())
+            for (i in a.indices)
+                if (i % 2 == 0) evens += a[i]
+                else odds += a[i]
+            evens = _fft(evens, direction, scalar)
+            odds = _fft(odds, direction, scalar)
+
+            val pairs = (0 until n / 2).map {
+                val offset = (direction * (java.lang.Math.PI * it / n)).exp * odds[it] / scalar
+                val base = evens[it] / scalar
+                Pair(base + offset, base - offset)
+            }
+            var (left, right) = Pair(emptyArray<Complex>(), emptyArray<Complex>())
+            for ((l, r) in pairs) { left += l; right += r }
+            left + right
+        }
+}
 
 class NewPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -124,6 +173,22 @@ class NewPage : AppCompatActivity() {
         // now bitmap holds the updated pixels
         // set bitmap as background to ImageView
         imageV.background = BitmapDrawable(getResources(), bitmap)
+
+        ////////////////////////////////////////////////////////////////////////////////////////
+
+        //Exemplu de fft
+        val dataInputFFT = arrayOf(Complex(1.0, 0.0), Complex(1.0, 0.0), Complex(1.0, 0.0), Complex(1.0, 0.0),
+            Complex(0.0, 0.0), Complex(0.0, 2.0), Complex(0.0, 0.0), Complex(0.0, 0.0))
+
+        val rezFFT = FFT.fft(dataInputFFT)
+
+        println("Exemplu FFT")
+        var j=0
+        while(j<rezFFT.size){
+            println(rezFFT[j])
+            j++
+        }
+
 
     }
 }
